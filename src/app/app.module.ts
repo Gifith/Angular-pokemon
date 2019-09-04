4f2de2d5b0a193bae05cd33e2d7a5fdb3d5eadf9
Maugard Martin (MoP/TEF8) <mau9mo@bosch.com>
2019-09-04 11:34:10 +0200

    PWA Completion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PokemonsModule } from './pokemons/pokemons.module';
+
 import { AppComponent } from './app.component';
 
-import { BorderCardDirective } from './border-card.directive';
-
-import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
-
 import { AppRoutingModule } from './app-routing.module';
-import { ListPokemonComponent } from './list-pokemon.component';
-import { DetailPokemonComponent } from './detail-pokemon.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BorderCardDirective,
-    PokemonTypeColorPipe,
-    ListPokemonComponent,
-    DetailPokemonComponent,
-    PageNotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
-  ],
+  declarations: [AppComponent, PageNotFoundComponent],
+  imports: [BrowserModule, PokemonsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
